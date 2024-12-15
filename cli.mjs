414b7edfac2b7c9c7481f9bef0a543d6aa397c11
Avaray <1459553+Avaray@users.noreply.github.com>
2024-12-15 15:17:40 +0100

    fixed error displaying

--- cli.mjs
+++ cli.mjs
@@ -4,9 +4,9 @@ import gip from "./module.mjs";
 import process from "node:process";
 
 try {
-  console.log(await gip(process.argv.slice(2)))
-  process.exit(0)
+  console.log(await gip(process.argv.slice(2)));
+  process.exit(0);
 } catch (error) {
-  console.log(console.error(error));
-  process.exit(1)
+  console.error(error.message);
+  process.exit(1);
 }
